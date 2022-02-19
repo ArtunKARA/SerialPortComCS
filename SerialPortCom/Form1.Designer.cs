82c950544daee615b217a7777ddf78f96f338727
ArtunKARA <artunankara@gmail.com>
2022-02-19 12:00:38 +0300

    Proje dosyası ekle.

---
+++ SerialPortCom/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace SerialPortCom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbComPort = new System.Windows.Forms.ComboBox();
+            this.cbBaundRate = new System.Windows.Forms.ComboBox();
+            this.cbDataBirts = new System.Windows.Forms.ComboBox();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
+            this.cbParityBits = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btSendData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cbParityBits);
+            this.groupBox1.Controls.Add(this.cbStopBits);
+            this.groupBox1.Controls.Add(this.cbDataBirts);
+            this.groupBox1.Controls.Add(this.cbBaundRate);
+            this.groupBox1.Controls.Add(this.cbComPort);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 248);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Com Port Control";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Com Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Baund Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data Bits";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Stop Bits";
+            // 
+            // cbComPort
+            // 
+            this.cbComPort.FormattingEnabled = true;
+            this.cbComPort.Location = new System.Drawing.Point(107, 34);
+            this.cbComPort.Name = "cbComPort";
+            this.cbComPort.Size = new System.Drawing.Size(121, 24);
+            this.cbComPort.TabIndex = 4;
+            // 
+            // cbBaundRate
+            // 
+            this.cbBaundRate.FormattingEnabled = true;
+            this.cbBaundRate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600"});
+            this.cbBaundRate.Location = new System.Drawing.Point(107, 66);
+            this.cbBaundRate.Name = "cbBaundRate";
+            this.cbBaundRate.Size = new System.Drawing.Size(121, 24);
+            this.cbBaundRate.TabIndex = 5;
+            this.cbBaundRate.Text = "9600";
+            // 
+            // cbDataBirts
+            // 
+            this.cbDataBirts.FormattingEnabled = true;
+            this.cbDataBirts.Items.AddRange(new object[] {
+            "6",
+            "7",
+            "8"});
+            this.cbDataBirts.Location = new System.Drawing.Point(107, 96);
+            this.cbDataBirts.Name = "cbDataBirts";
+            this.cbDataBirts.Size = new System.Drawing.Size(121, 24);
+            this.cbDataBirts.TabIndex = 6;
+            this.cbDataBirts.Text = "8";
+            // 
+            // cbStopBits
+            // 
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Items.AddRange(new object[] {
+            "One",
+            "Two"});
+            this.cbStopBits.Location = new System.Drawing.Point(107, 126);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(121, 24);
+            this.cbStopBits.TabIndex = 7;
+            this.cbStopBits.Text = "One";
+            // 
+            // cbParityBits
+            // 
+            this.cbParityBits.FormattingEnabled = true;
+            this.cbParityBits.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even"});
+            this.cbParityBits.Location = new System.Drawing.Point(107, 156);
+            this.cbParityBits.Name = "cbParityBits";
+            this.cbParityBits.Size = new System.Drawing.Size(121, 24);
+            this.cbParityBits.TabIndex = 8;
+            this.cbParityBits.Text = "None";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Parity Bıts";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btClose);
+            this.groupBox2.Controls.Add(this.btOpen);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 266);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(206, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(6, 21);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(75, 23);
+            this.btOpen.TabIndex = 1;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(137, 21);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 2;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // btSendData
+            // 
+            this.btSendData.Location = new System.Drawing.Point(236, 266);
+            this.btSendData.Name = "btSendData";
+            this.btSendData.Size = new System.Drawing.Size(88, 100);
+            this.btSendData.TabIndex = 2;
+            this.btSendData.Text = "Send Data";
+            this.btSendData.UseVisualStyleBackColor = true;
+            this.btSendData.Click += new System.EventHandler(this.btSendData_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(330, 20);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 307);
+            this.textBox1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 205);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(133, 205);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 20);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 526);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btSendData);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "C# COM PORT SERIAL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbParityBits;
+        private System.Windows.Forms.ComboBox cbStopBits;
+        private System.Windows.Forms.ComboBox cbDataBirts;
+        private System.Windows.Forms.ComboBox cbBaundRate;
+        private System.Windows.Forms.ComboBox cbComPort;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btSendData;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ SerialPortCom/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPortCom
+{
+    public partial class Form1 : Form
+    {
+        string dataOut;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cbComPort.Items.AddRange(ports);
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cbComPort.Text;
+                serialPort1.BaudRate = Convert.ToInt16(cbBaundRate.Text);
+                serialPort1.DataBits = Convert.ToInt16(cbDataBirts.Text);
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cbStopBits.Text);
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), cbParityBits.Text);
+
+
+                serialPort1.Open();
+                progressBar1.Value = 100;
+            }
+            catch (Exception err)
+            {
+
+                MessageBox.Show(err.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                progressBar1.Value=0;
+            }
+        }
+
+        private void btSendData_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                dataOut = textBox1.Text;
+                serialPort1.WriteLine(dataOut);
+            }
+        }
+    }
+}
